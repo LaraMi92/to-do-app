04c859a431967470a771305e495d77ee550619c0
LaraMi92 <laraminski@hotmail.fr>
2020-11-09 17:29:18 +0100

    tests

--- sequelizeTests.js
+++ sequelizeTests.js
@@ -3,6 +3,10 @@ const {List, Card, Tag} = require('./app/models');
 
 /* List.findByPk(2,console.log); */
 
-Card.findOne({
+/* Card.findOne({
     where: {position: 1}
+}).then(card => console.log(card)); */
+
+Card.findAll({
+    include : {association: 'tags'}
 }).then(card => console.log(card));
